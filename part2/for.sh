2bc36ee77452a900750c6c5cf37fb5198bcb846e
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2018-04-08 16:18:36 +0900

    20180407 commit

---
+++ part2/for.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for fruit in apples oranges pears bananas
+do
+echo $fruit
+done
+
+echo "script complete"
---
+++ part2/testfunc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+lister() {
+ echo "current directory is 'pwd' ."
+ echo "current directory's files are the following."
+ ls
+}
+
+lister
